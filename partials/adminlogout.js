5a4ecdd2107a94c466c3e93bf30a88831693b8d4
Ekta Verma <164794593+EktaVerma24@users.noreply.github.com>
2024-07-29 23:20:11 +0530

    Update adminlogout.js

--- partials/adminlogout.js
+++ partials/adminlogout.js
@@ -2,6 +2,6 @@
 function confirmLogout(event) {
     event.preventDefault(); // Prevent the default link behavior
     if (confirm("Are you sure you want to log out?")) {
-        window.location.href = 'adminlogout.php'; // Redirect to logout.php if confirmed
+        window.location.href = 'index.html'; // Redirect to logout.php if confirmed
     }
 }
